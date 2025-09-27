8563899882af491e2615d4a9ae6b727d546fc92f
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 08:32:31 +0530

    messages

--- catalogue.sh
+++ catalogue.sh
@@ -6,21 +6,7 @@ app_setup
 nodejs_setup
 systemd_setup
 
-cp $SCRIPT_DIR/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-VALIDATE $? "copy mongo repo"
-
-dnf install mongodb-mongosh -y &>>$LOG_FILE
-VALIDATE $? "install mongodb client"
-
-INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
-if [ $INDEX -le 0 ]; then
-    mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
-    VALIDATE $? "Load catalogue products"
-else
-    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
-fi
-#mongosh --host $MONGODB_HOST </app/db/master-data.js &>> LOG_FILE
-#VALIDATE $? "load catalogue products"
+catalogue_setup
 
 app_restart
 
