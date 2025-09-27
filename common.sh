6c268dd26c283f406c04b688ce5e484194b9030b
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 09:22:09 +0530

    messages

--- common.sh
+++ common.sh
@@ -200,6 +200,32 @@ nginx_setup () {
     dnf install nginx -y &>>$LOG_FILE
     VALIDATE $? "install nginx"
 }
+
+frontend_setup () {
+    systemctl enable nginx  &>>$LOG_FILE
+    VALIDATE $? "enable nginx"
+
+    systemctl start nginx  &>>$LOG_FILE
+    VALIDATE $? "start nginx"
+
+    rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
+    VALIDATE $? "remove old code"
+
+    curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
+    VALIDATE $? "download app code"
+
+    cd /usr/share/nginx/html  &>>$LOG_FILE
+    VALIDATE $? "changing to directory"
+
+    unzip /tmp/frontend.zip &>>$LOG_FILE
+    VALIDATE $? "unzip code"
+
+    cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
+    VALIDATE $? "cp systemd service"
+
+    systemctl restart nginx &>>$LOG_FILE
+    VALIDATE $? "restart nginx"
+}
 app_restart () {
     systemctl restart $app_name
     VALIDATE $? "restart $app_name"
--- frontend.sh
+++ frontend.sh
@@ -2,31 +2,8 @@
 source ./common.sh
 check_root
 
-
 nginx_setup
 
-systemctl enable nginx  &>>$LOG_FILE
-VALIDATE $? "enable nginx"
-
-systemctl start nginx  &>>$LOG_FILE
-VALIDATE $? "start nginx"
-
-rm -rf /usr/share/nginx/html/*  &>>$LOG_FILE
-VALIDATE $? "remove old code"
-
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip &>>$LOG_FILE
-VALIDATE $? "download app code"
-
-cd /usr/share/nginx/html  &>>$LOG_FILE
-VALIDATE $? "changing to directory"
-
-unzip /tmp/frontend.zip &>>$LOG_FILE
-VALIDATE $? "unzip code"
-
-cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf &>>$LOG_FILE
-VALIDATE $? "cp systemd service"
-
-systemctl restart nginx &>>$LOG_FILE
-VALIDATE $? "restart nginx"
+frontend_setup
 
 print_total_time
