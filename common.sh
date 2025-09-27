d0f7d37ba64288a8b10bf7b4b120f75e17329ea8
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 08:29:45 +0530

    messages

--- common.sh
+++ common.sh
@@ -85,6 +85,22 @@ systemd_setup () {
     systemctl start $app_name
     VALIDATE $? "start $app_name"  
 }
+mongodb_setup () {
+    cp mongo.repo /etc/yum.repos.d/mongo.repo
+    VALIDATE $? "Adding Mongo repo"
+
+    dnf install mongodb-org -y &>>$LOG_FILE
+    VALIDATE $? "Installing MongoDB"
+
+    systemctl enable mongod &>>$LOG_FILE
+    VALIDATE $? "Enable MongoDB"
+
+    systemctl start mongod 
+    VALIDATE $? "Start MongoDB"
+
+    sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+    VALIDATE $? "Allowing remote connections to MongoDB"
+}
 
 redis_setup () {
     dnf module disable redis -y  &>>$LOG_FILE
--- mongodb.sh
+++ mongodb.sh
@@ -4,22 +4,8 @@ app_name=mongodb
 
 check_root
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo
-VALIDATE $? "Adding Mongo repo"
+mongodb_setup
 
-dnf install mongodb-org -y &>>$LOG_FILE
-VALIDATE $? "Installing MongoDB"
-
-systemctl enable mongod &>>$LOG_FILE
-VALIDATE $? "Enable MongoDB"
-
-systemctl start mongod 
-VALIDATE $? "Start MongoDB"
-
- sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
- VALIDATE $? "Allowing remote connections to MongoDB"
-
-systemctl restart mongod
-VALIDATE $? "Restarted MongoDB"
+app_restart
 
 print_total_time
