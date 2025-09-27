0074ac17bb308f5335f20c9d5e1acbce86f4a844
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 07:56:00 +0530

    messages

--- common.sh
+++ common.sh
@@ -36,6 +36,60 @@ VALIDATE () {
     fi
 }
 
+nodejs_setup() {
+    dnf module disable nodejs -y &>>$LOG_FILE
+    VALIDATE $? "disable nodejs"
+
+    dnf module enable nodejs:20 -y &>>$LOG_FILE
+    VALIDATE $? "enable nodejs"
+
+    dnf install nodejs -y &>>$LOG_FILE
+    VALIDATE $? "install nodejs"
+
+    npm install &>>$LOG_FILE
+    VALIDATE $? "install dependencies"
+}
+
+app_setup () {
+    id roboshop &>>$LOG_FILE
+    if [ $? -ne 0 ]; then
+        useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+        VALIDATE $? "creating system user" &>>$LOG_FILE
+    else
+        echo -e " user is already exist ... $Y SKIPPING $N"
+    fi
+
+    mkdir -p /app 
+    VALIDATE $? "creating app directory"
+
+    curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/$app_name-v3.zip &>>$LOG_FILE
+    VALIDATE $? "download code"
+    cd /app
+    VALIDATE $? "changing app directory" 
+
+    rm -rf /app/*
+    VALIDATE $? "remove old code"
+
+    unzip /tmp/$app_name.zip &>>$LOG_FILE
+    VALIDATE $? "unzip code"
+}
+
+systemd_setup () {
+    cp $SCRIPT_DIR/$app_name.service /etc/systemd/system/$app_name.service &>>$LOG_FILE
+    VALIDATE $? "copy systemctl service"
+
+    systemctl daemon-reload &>>$LOG_FILE
+    systemctl enable $app_name &>>$LOG_FILE
+    VALIDATE $? "enable $app_name" 
+
+    systemctl start $app_name
+    VALIDATE $? "start $app_name"  
+}
+
+systemctl_restart () {
+    systemctl restart $app_name
+    VALIDATE $? "restart $app_name"
+}
 print_total_time () {
     END_TIME=$(date +%s)
     TOTAL_TIME=$(( $END_TIME - $START_TIME))
--- mongodb.sh
+++ mongodb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 source ./common.sh
-app_name=catalogue
+app_name=mongodb
 
 check_root
 
