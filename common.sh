ccecca74e4ddc635a02278b2ad32618cfc61a15e
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 08:24:38 +0530

    messages

--- common.sh
+++ common.sh
@@ -86,6 +86,18 @@ systemd_setup () {
     VALIDATE $? "start $app_name"  
 }
 
+mysql_setup () {
+    dnf install mysql-server -y &>>$LOG_FILE
+    VALIDATE $? "install mysql"
+
+    systemctl enable mysqld &>>$LOG_FILE
+    VALIDATE $? "enable mysql"
+
+    systemctl start mysqld &>>$LOG_FILE
+    VALIDATE $? "start mysql"
+    mysql_secure_installation --set-root-pass RoboShop@1 &>>$LOG_FILE
+    VALIDATE $? "set password"
+}
 app_restart () {
     systemctl restart $app_name
     VALIDATE $? "restart $app_name"
