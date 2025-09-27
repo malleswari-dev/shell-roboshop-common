f71184cc0ed5a8094526c5b1a80076e0f1e71ed3
malleswari <maddikirimalleswari4@gmail.com>
2025-09-27 08:27:04 +0530

    messages

--- common.sh
+++ common.sh
@@ -86,6 +86,26 @@ systemd_setup () {
     VALIDATE $? "start $app_name"  
 }
 
+redis_setup () {
+    dnf module disable redis -y  &>>$LOG_FILE
+    VALIDATE $? "disabling redis"
+
+    dnf module enable redis:7 -y &>>$LOG_FILE
+    VALIDATE $? "enabling redis 7"
+
+    dnf install redis -y &>>$LOG_FILE
+    VALIDATE $? "install redis"
+
+    sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>$LOG_FILE
+    VALIDATE $? "allowing remote connections to redis"
+
+    systemctl enable redis &>>$LOG_FILE
+    VALIDATE $? "enable redis"
+
+    systemctl start redis &>>$LOG_FILE
+    VALIDATE $? "start redis"
+}
+
 mysql_setup () {
     dnf install mysql-server -y &>>$LOG_FILE
     VALIDATE $? "install mysql"
